473ee42c28505dd9c65dbbf4b973a977986ccc3f
slavlee <info@slavlee.de>
2021-12-23 13:08:46 +0100

    - [NEW] Registrierung Backend Modul für Dashboard

--- advertisement/ext_tables.php
+++ advertisement/ext_tables.php
@@ -2,6 +2,9 @@
 defined('TYPO3') || die();
 
 (static function() {
+	/***************************************************************
+	 * NEW MODELS - START
+	 **************************************************************/
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_advertisement_domain_model_campaign', 'EXT:advertisement/Resources/Private/Language/locallang_csh_tx_advertisement_domain_model_campaign.xlf');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_advertisement_domain_model_campaign');
 
@@ -10,4 +13,30 @@ defined('TYPO3') || die();
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_advertisement_domain_model_zone', 'EXT:advertisement/Resources/Private/Language/locallang_csh_tx_advertisement_domain_model_zone.xlf');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_advertisement_domain_model_zone');
+    /***************************************************************
+     * NEW MODELS - END
+     **************************************************************/
+    
+    /***************************************************************
+     * BACKEND MODULES - START
+     **************************************************************/
+    // Module System > Backend Users
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+   		'Advertisement',
+   		'tools',
+   		'Dashboard',
+   		'bottom',
+  		[
+			\Slavlee\Advertisement\Controller\Backend\DashboardController::class => 'show'
+   		],
+  		[
+			'access' => 'admin',
+			'iconIdentifier' => 'apps-pagetree-folder-contains-advertisement',
+			'labels' => 'LLL:EXT:advertisement/Resources/Private/Language/locallang_mod.xlf',
+			'inheritNavigationComponentFromMainModule' => false,
+   		]
+	);
+    /***************************************************************
+     * BACKEND MODULES - END
+     **************************************************************/
 })();
